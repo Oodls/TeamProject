eba0e3940e42e034d233701fa51b73eeb3d78caf
odls <leesj000603@daum.net>
2025-05-07 09:46:36 +0900

    fix

--- src/main/java/com/example/teamproject/controller/IndexController.java
+++ src/main/java/com/example/teamproject/controller/IndexController.java
@@ -1,14 +1,15 @@
-package com.example.controller;
+package com.example.teamproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public @ResponseBody String index() {
+    public @ResponseBody String getMethodName(@RequestParam String param) {
         return "ㅎㅇ";
     }
     
